b7a8cbe1cffb83caf0c07db4245dcbdfec9ac7c9
Helen Qu <helenqu@midway2-login2.rcc.local>
2021-03-10 12:55:01 -0600

    incorporated mode (train/predict), added run method to create_heatmaps_utils, changed create_heatmaps from sbatch to multiprocessing

--- create_heatmaps.py
+++ create_heatmaps.py
@@ -26,8 +26,12 @@ def load_config(config_path):
 
 # print(args.config_path)
 # print(os.path.abspath(args.config_path))
+# if config["input_path"]:
+#     config['metadata_paths'] = [f.path for f in os.scandir(config["input_path"]) if "HEAD.csv" in f.name]
+#     config['lcdata_paths'] = [path.replace("HEAD", "PHOT") for path in config['metadata_paths']]
+#     write_config(config, args.config_path)
+
 config = load_config(args.config_path)
-<<<<<<< HEAD
 num_paths = len(config["lcdata_paths"])
 
 procs = []
@@ -112,22 +116,50 @@ sbatch_header = """#!/bin/bash
 
 if NUM_PATHS > 1:
     sbatch_header += "\n#SBATCH --array=0-{}".format(NUM_PATHS-1)
+=======
+>>>>>>> incorporated mode (train/predict), added run method to create_heatmaps_utils, changed create_heatmaps from sbatch to multiprocessing
 else:
-    sbatch_header += "\n#SBATCH --array=0"
+    donefile_info = "indices of failed create heatmaps jobs: {}\ncheck out the LC data files or metadata files at those indices in the config yml at {}\nlogs located at create_heatmaps_i.log, i=failed index".format(failed_procs, args.config_path)
+    exit_code = 1
 
-task = """source activate scone_cpu
-python create_heatmaps_utils.py --config_path {} --index $SLURM_ARRAY_TASK_ID""".format(CONFIG_PATH)
+with open(config["donefile"], "w+") as donefile:
+    donefile.write(donefile_info)
 
-slurm = """{sbatch_header}
-{task}
-"""
+sys.exit(exit_code)
 
-format_dict = {"sbatch_header": sbatch_header, "task": task}
-final_slurm = slurm.format(**format_dict)
-with open("./slurm.job", "w+") as batchfile:
-    batchfile.write(final_slurm)
+## TODO: find out how to use pippin sbatch header
+# SBATCH VERSION
+#    print("sbatch")
+#    sbatch_header = """#!/bin/bash
 
-subprocess.Popen("sbatch slurm.job".split())
+#    #SBATCH --partition=broadwl
+#    #SBATCH --account=pi-rkessler
+#    #SBATCH --job-name=create_heatmaps
+#    #SBATCH --output=test_sbatch_heatmaps.log
+#    #SBATCH --time=00:10:00
+#    #SBATCH --nodes=1
+#    #SBATCH --mem-per-cpu=1000
+#    #SBATCH --exclusive
+#    #SBATCH --ntasks-per-node=1"""
+
+#    if NUM_PATHS > 1:
+#        sbatch_header += "\n#SBATCH --array=0-{}".format(NUM_PATHS-1)
+#    else:
+#        sbatch_header += "\n#SBATCH --array=0"
+
+#    task = """source activate scone_cpu
+#    python create_heatmaps_utils.py --config_path {} --index $SLURM_ARRAY_TASK_ID""".format(CONFIG_PATH)
+
+#    slurm = """{sbatch_header}
+#    {task}
+#    """
+
+#    format_dict = {"sbatch_header": sbatch_header, "task": task}
+#    final_slurm = slurm.format(**format_dict)
+#    with open("./slurm.job", "w+") as batchfile:
+#        batchfile.write(final_slurm)
+#    subprocess.run(["sbatch", "slurm.job"])
+#else:
 #for i in range(NUM_PATHS):
 #    #TODO: determine a good way to estimate time
 #    cmd = "srun -C haswell -q regular -N 1 --time 01:00:00 {} {} {} &".format(SCRIPT_PATH, CONFIG_PATH, i)
--- create_heatmaps_utils.py
+++ create_heatmaps_utils.py
@@ -158,9 +158,16 @@ def image_example(image_string, label, id):
     example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString()
 
+<<<<<<< HEAD
 # LOAD DATA
 def run(config, index):
     # sys.stdout = open(os.path.join(config["heatmaps_path"], "create_heatmaps_{}.log".format(index)), "w+")
+=======
+
+# LOAD DATA
+def run(config, index):
+    sys.stdout = open("create_heatmaps_{}.log".format(index), "w+")
+>>>>>>> incorporated mode (train/predict), added run method to create_heatmaps_utils, changed create_heatmaps from sbatch to multiprocessing
 
     METADATA_PATH = config["metadata_paths"][index]
     LCDATA_PATH = config["lcdata_paths"][index]
@@ -181,10 +188,18 @@ def run(config, index):
         ids_file = h5py.File(IDS_PATH, "r")
         ids = [x.decode('utf-8') for x in ids_file["names"]]
         ids_file.close()
+<<<<<<< HEAD
         print("job {}: found ids, expect {} total heatmaps".format(index, len(ids)), flush=True)
     else:
         ids = None
         print("job {}: no ids, expect {} total heatmaps".format(index, len(lcdata_ids)), flush=True)
+=======
+        print("expect {} total heatmaps".format(len(ids)), flush=True)
+    else:
+        ids = None
+        print("expect {} total heatmaps".format(len(lcdata_ids)), flush=True)
+    print(tf.__version__)
+>>>>>>> incorporated mode (train/predict), added run method to create_heatmaps_utils, changed create_heatmaps from sbatch to multiprocessing
 
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
@@ -197,8 +212,15 @@ def run(config, index):
 
     with tf.io.TFRecordWriter("{}/heatmaps_{}.tfrecord".format(OUTPUT_PATH, index)) as writer:
         for i, sn_id in enumerate(lcdata_ids):
+<<<<<<< HEAD
             if i % 1000 == 0:
                 print("job {}: processing {} of {}".format(index, i, len(lcdata_ids)), flush=True)
+=======
+            if i > 1000:
+                break
+            # if i % 1000 == 0:
+            #     print("processing {} of {}".format(i, len(lcdata_ids)), flush=True)
+>>>>>>> incorporated mode (train/predict), added run method to create_heatmaps_utils, changed create_heatmaps from sbatch to multiprocessing
             sn_id = int(sn_id)
             sn_metadata = metadata[metadata.object_id == sn_id]
 
