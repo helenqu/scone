5836a894a6ae957f9277c1bc9b2f7ca23172be6f
Helen Qu <helenqu@midway2-login1.rcc.local>
2021-03-23 13:37:18 -0500

    OOPify

--- model_utils.py
+++ model_utils.py
@@ -1,104 +1,164 @@
 import os
 import numpy as np
+import yaml
 import tensorflow as tf
 from tensorflow.keras import layers, models, utils, optimizers
 import h5py
 
 from data_utils import *
 
-# defines and compiles, then returns model
-# requires:
-#   - INPUT_SHAPE
-#   - CATEGORICAL
-#   - NUM_TYPES
-def define_and_compile_model(input_shape, categorical, num_types):
-    y, x, _ = input_shape
-    
-    model = models.Sequential()
+class SconeClassifier():
+    def __init__(self, config):
+        self.heatmaps_path = config['heatmaps_path']
+        self.output_path = config['output_path']
+        self.batch_size = config.get('batch_size', 32)
+        self.num_epochs = config['num_epochs']
+        self.input_shape = (config['num_wavelength_bins'], config['num_mjd_bins'], 2)
+        self.categorical = config['categorical']
+        self.num_types = config.get('num_types', None)
+        if self.categorical and not self.num_types:
+            raise KeyError('cannot perform categorical classification without knowing the number of source types! please specify the `num_types` key in your config file to reflect this information')
+            # TODO: should i write num types info into a file after create heatmaps? maybe ids file will be large
+            # ids_file = h5py.File(config['ids_path'], "r")
+            # types = [x.decode('utf-8').split("_")[0] for x in ids_file["names"]]
+            # ids_file.close()
+            # self.num_types = len(np.unique(types))
+        self.train_proportion = config.get('train_proportion', 0.8)
+        self.use_test_set = True if config["mode"] == "predict" else False
+        self.trained_model = None # TODO: or the location of a saved trained model
+        self.train_set, self.val_set, self.test_set, self.train_ids, self.val_ids, self.test_ids = self._split_and_retrieve_data()
 
-    model.add(layers.ZeroPadding2D(padding=(0,1), input_shape=input_shape))
-    model.add(layers.Conv2D(y, (y, 3), activation='elu'))
-    model.add(layers.Reshape((y, x, 1)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ZeroPadding2D(padding=(0,1)))
-    model.add(layers.Conv2D(y, (y, 3), activation='elu'))
-    model.add(layers.Reshape((y, x, 1)))
-    model.add(layers.BatchNormalization())
+    def run(self):
+        if not self.trained_model:
+            _, history = self.train()
+        dataset, dataset_ids = self.get_test_set() if self.use_test_set else self.get_train_set()
+        preds_dict = self.predict(dataset, dataset_ids)
 
-    model.add(layers.MaxPooling2D((2, 2)))
-    
-    model.add(layers.ZeroPadding2D(padding=(0,1)))
-    model.add(layers.Conv2D(int(y/2), (int(y/2), 3), activation='elu'))
-    model.add(layers.Reshape((int(y/2), int(x/2), 1)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ZeroPadding2D(padding=(0,1)))
-    model.add(layers.Conv2D(int(y/2), (int(y/2), 3), activation='elu'))
-    model.add(layers.Reshape((int(y/2), int(x/2), 1)))
-    model.add(layers.BatchNormalization())
+        if self.use_test_set:
+            test_acc = self.test()
+            history.history["test_accuracy"] = test_acc
 
-    model.add(layers.MaxPooling2D((2, 2)))
+        return preds_dict, history
 
-    model.add(layers.Flatten())
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dropout(0.3))
+    # train the model, returns trained model & training log
+    # requires:
+    #   - model
+    #   - train_set, val_set
+    #   - NUM_EPOCHS
+    #   - batch_size
+    def train(self):
+        model = self._define_and_compile_model()
+        history = model.fit(
+            self.train_set,
+            epochs=self.num_epochs,
+            validation_data=self.val_set,
+            verbose=0)
 
-    if categorical:
-        model.add(layers.Dense(num_types, activation='softmax'))
-    else:
-        model.add(layers.Dense(1, activation='sigmoid'))
-    
-    opt = optimizers.Adam(learning_rate=1e-3)
-    loss = 'sparse_categorical_crossentropy' if categorical else 'binary_crossentropy'
-    model.compile(optimizer=opt,
-                  loss=loss,
-                  metrics=['accuracy'])
+        self.trained_model = model
+        return model, history
 
-    return model
+    def predict(self, dataset, dataset_ids):
+        if not self.trained_model:
+            raise RuntimeError('model has not been trained! call `train` on the SconeClassifier instance before predict!')
+        predictions = self.trained_model.predict(dataset, verbose=0)
+        if self.categorical:
+            predictions = np.argmax(predictions, axis=1) #TODO: is this the best way to return categorical results? doesnt preserve confidence info
+        predictions = predictions.flatten()
+        df_dict = {'snid': dataset_ids, 'pred': predictions}
+        return df_dict
 
-# main data retrieval function
-# requires:
-#   - heatmaps_path
-#   - get_images
-#   - stratified_split
-#   - extract_ids
-def split_and_retrieve_data(heatmaps_path, train_proportion, include_test_set, input_shape, categorical, batch_size):
-    raw_dataset = tf.data.TFRecordDataset(
-        ["{}/{}".format(heatmaps_path, f.name) for f in os.scandir(heatmaps_path) if "tfrecord" in f.name], 
-        num_parallel_reads=80)
-    dataset = raw_dataset.map(lambda x: get_images(x, input_shape, categorical), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.apply(tf.data.experimental.ignore_errors())
+    def test(self):
+        if not self.use_test_set:
+            raise RuntimeError('no testing in train mode')
+        if not self.trained_model:
+            raise RuntimeError('model has not been trained! call `train` on the SconeClassifier instance before test!')
+        results = self.trained_model.evaluate(self.test_set)
+        return results[1]
 
-    train_set, val_set, test_set = stratified_split(dataset, train_proportion, include_test_set)
-    train_set = train_set.prefetch(tf.data.experimental.AUTOTUNE).cache()
-    val_set = val_set.prefetch(tf.data.experimental.AUTOTUNE).cache()
-    test_set = test_set.prefetch(tf.data.experimental.AUTOTUNE).cache() if test_set else None
+    def get_train_set(self):
+        if not self.train_set:
+            self.train_set, self.val_set, self.test_set, self.train_ids, self.val_ids, self.test_ids = self._split_and_retrieve_data()
+        return self.train_set, self.train_ids
 
-    return extract_ids_and_batch(train_set, val_set, test_set, batch_size)
+    def get_val_set(self):
+        if not self.val_set:
+            self.train_set, self.val_set, self.test_set, self.train_ids, self.val_ids, self.test_ids = self._split_and_retrieve_data()
+        return self.val_set, self.val_ids
 
-# train the model, returns trained model & training log
-# requires:
-#   - model
-#   - train_set, val_set
-#   - NUM_EPOCHS
-#   - batch_size
-def train(model, train_set, val_set, num_epochs):
-    history = model.fit(
-        train_set,
-        epochs=num_epochs,
-        validation_data=val_set,
-        verbose=0)
+    def get_test_set(self):
+        if not self.use_test_set:
+            raise RuntimeError('no test set in train mode')
+        if not self.test_set:
+            self.train_set, self.val_set, self.test_set, self.train_ids, self.val_ids, self.test_ids = self._split_and_retrieve_data()
+        return self.test_set, self.test_ids
 
-    return model, history
+    # defines and compiles, then returns model
+    # requires:
+    #   - INPUT_SHAPE
+    #   - CATEGORICAL
+    #   - NUM_TYPES
+    def _define_and_compile_model(self):
+        y, x, _ = self.input_shape
+        
+        model = models.Sequential()
 
-def get_predictions(model, dataset, dataset_ids, categorical):
-    predictions = model.predict(dataset, verbose=0)
-    if categorical:
-        predictions = np.argmax(predictions, axis=1) #TODO: is this the best way to return categorical results?
-    predictions = predictions.flatten()
-    df_dict = {'snid': dataset_ids, 'pred': predictions}
-    return df_dict
+        model.add(layers.ZeroPadding2D(padding=(0,1), input_shape=self.input_shape))
+        model.add(layers.Conv2D(y, (y, 3), activation='elu'))
+        model.add(layers.Reshape((y, x, 1)))
+        model.add(layers.BatchNormalization())
+        model.add(layers.ZeroPadding2D(padding=(0,1)))
+        model.add(layers.Conv2D(y, (y, 3), activation='elu'))
+        model.add(layers.Reshape((y, x, 1)))
+        model.add(layers.BatchNormalization())
 
-def test(model, test_set):
-    results = model.evaluate(test_set)
-    return results[1]
+        model.add(layers.MaxPooling2D((2, 2)))
+        
+        model.add(layers.ZeroPadding2D(padding=(0,1)))
+        model.add(layers.Conv2D(int(y/2), (int(y/2), 3), activation='elu'))
+        model.add(layers.Reshape((int(y/2), int(x/2), 1)))
+        model.add(layers.BatchNormalization())
+        model.add(layers.ZeroPadding2D(padding=(0,1)))
+        model.add(layers.Conv2D(int(y/2), (int(y/2), 3), activation='elu'))
+        model.add(layers.Reshape((int(y/2), int(x/2), 1)))
+        model.add(layers.BatchNormalization())
+
+        model.add(layers.MaxPooling2D((2, 2)))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dropout(0.5))
+        model.add(layers.Dense(32, activation='relu'))
+        model.add(layers.Dropout(0.3))
+
+        if self.categorical:
+            model.add(layers.Dense(self.num_types, activation='softmax'))
+        else:
+            model.add(layers.Dense(1, activation='sigmoid'))
+        
+        opt = optimizers.Adam(learning_rate=1e-3)
+        loss = 'sparse_categorical_crossentropy' if self.categorical else 'binary_crossentropy'
+        model.compile(optimizer=opt,
+                      loss=loss,
+                      metrics=['accuracy'])
+
+        return model
+
+    # main data retrieval function
+    # requires:
+    #   - heatmaps_path
+    #   - get_images
+    #   - stratified_split
+    #   - extract_ids
+    def _split_and_retrieve_data(self):
+        raw_dataset = tf.data.TFRecordDataset(
+            ["{}/{}".format(self.heatmaps_path, f.name) for f in os.scandir(self.heatmaps_path) if "tfrecord" in f.name], 
+            num_parallel_reads=80)
+        dataset = raw_dataset.map(lambda x: get_images(x, self.input_shape, self.categorical), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.apply(tf.data.experimental.ignore_errors())
+
+        train_set, val_set, test_set = stratified_split(dataset, self.train_proportion, self.use_test_set)
+        train_set = train_set.prefetch(tf.data.experimental.AUTOTUNE).cache()
+        val_set = val_set.prefetch(tf.data.experimental.AUTOTUNE).cache()
+        test_set = test_set.prefetch(tf.data.experimental.AUTOTUNE).cache() if self.use_test_set else None
+
+        return extract_ids_and_batch(train_set, val_set, test_set, self.batch_size)
+
--- run_model.py
+++ run_model.py
@@ -1,9 +1,7 @@
 import os
-from model_utils import *
-import argparse
+from model_utils import SconeClassifier
 import yaml
-import h5py
-import numpy as np
+import argparse
 import pandas as pd
 import time
 
@@ -11,46 +9,28 @@ start = time.time()
 # GET CONFIG PATH
 parser = argparse.ArgumentParser(description='set up the SCONE model')
 parser.add_argument('--config_path', type=str, help='absolute or relative path to your yml config file, i.e. "/user/files/config.yml"')
-parser.add_argument('--mode', type=str, help='train or predict')
 args = parser.parse_args()
 
-# LOAD CONFIG
 with open(args.config_path, "r") as cfgfile:
     config = yaml.load(cfgfile)
 
-# DEFINE PARAMS
-HEATMAPS_PATH = config['heatmaps_path']
-OUTPUT_PATH = config['output_path']
-BATCH_SIZE = config['batch_size'] if 'batch_size' in config else 32
-NUM_EPOCHS = config['num_epochs']
-INPUT_SHAPE = (config['num_wavelength_bins'], config['num_mjd_bins'], 2)
-CATEGORICAL = config['categorical']
-NUM_TYPES = None
-if CATEGORICAL:
-    # TODO: should i write num types info into a file after create heatmaps? maybe ids file will be large
-    ids_file = h5py.File(config['ids_path'], "r")
-    types = [x.decode('utf-8').split("_")[0] for x in ids_file["names"]]
-    ids_file.close()
-    NUM_TYPES = len(np.unique(types))
-TRAIN_PROPORTION = config['train_proportion'] if 'train_proportion' in config else 0.8
-PREDICT = True if config["mode"] == "predict" else False
+# TRAIN + TEST
+# model = SconeClassifier(args.config_path)
+# _, history = model.train()
+# pd.DataFrame(history.history).to_csv(os.path.join(OUTPUT_PATH, "training_history.csv"))
+# test_acc = model.test()
 
-# EXECUTE
-model = define_and_compile_model(INPUT_SHAPE, CATEGORICAL, NUM_TYPES)
-#TODO: maybe split out the test set processing
-train_set, val_set, test_set, train_ids, val_ids, test_ids = split_and_retrieve_data(HEATMAPS_PATH, TRAIN_PROPORTION, PREDICT, INPUT_SHAPE, CATEGORICAL, BATCH_SIZE)
-model, history = train(model, train_set, val_set, NUM_EPOCHS)
-pd.DataFrame(history.history).to_csv(os.path.join(OUTPUT_PATH, "training_history.csv"))
+# TRAIN + PREDICT
+# trained_model, history = model.train()
+# test_set, test_ids = model.get_test_set()
+# trained_model.predict(test_set, test_ids)
 
+preds_dict, history = SconeClassifier(config).run()
 print("######## CLASSIFICATION REPORT ########")
 print("classification finished in {:.2f}min".format((time.time() - start) / 60))
 print("last training accuracy value: {}".format(history.history["accuracy"][-1]))
 print("last validation accuracy value: {}".format(history.history["val_accuracy"][-1]))
-if PREDICT:
-    acc = test(model, test_set)
-    print("test accuracy: {}\n".format(acc))
-    preds_dict = get_predictions(model, test_set, test_ids, CATEGORICAL)
-else:
-    preds_dict = get_predictions(model, train_set, train_ids, CATEGORICAL)
+if "test_accuracy" in history.history:
+    print("test accuracy value: {}".format(history.history["test_accuracy"]))
 
-pd.DataFrame(preds_dict).to_csv(os.path.join(OUTPUT_PATH, "predictions.csv"), index=False)
+pd.DataFrame(preds_dict).to_csv(os.path.join(config['output_path'], "predictions.csv"), index=False)
